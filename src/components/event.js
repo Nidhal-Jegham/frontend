ff7c352844021af6e47f546aab011c61e188eecc
Nidhal-Jegham <jeghamnidhal7@gmail.com>
2022-08-31 18:26:14 +0100

    fez

--- src/components/event.js
+++ src/components/event.js
@@ -11,7 +11,7 @@ const Event = (props) => {
         </div>
         <div class="card-body">
           <h5 class="card-title">
-            <a href="#">{props.event.name}</a>
+            <a href={props.event.drive}>{props.event.name}</a>
           </h5>
           <p class="card-text">{props.event.description}</p>
 
