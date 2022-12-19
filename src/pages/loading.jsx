f40b62645425b3b0d5732991d11e7a2f11fbe099
nidhal jegham <jeghamnidhal7@gmail.com>
2022-12-19 18:53:30 +0100

    0.92

--- src/pages/loading.jsx
+++ src/pages/loading.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "../styles/loading.scss";
 const Loading = () => {
   return (
-    <div>
-      <img className="loading" src="/assets/img/Logo.png"></img>
+    <div className="notfound ">
+      <img src="/assets/img/Logo.png"></img>
     </div>
   );
 };
